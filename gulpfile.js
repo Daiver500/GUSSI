741dd5c1354dde6a51d1488ff8fb17453916e000
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-10-20 17:50:31 +0300

    adaptive done

--- gulpfile.js
+++ gulpfile.js
@@ -114,7 +114,7 @@ const build = () => gulp.series (
   styles,
   images,
   //sprite,
-  //htmlinclude,
+  htmlinclude,
   imageswebp,
 );
 
@@ -153,7 +153,7 @@ exports.html = html;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", gulp.series("html", /*"htmlinclude",*/ sync.reload));
+  gulp.watch("source/*.html").on("change", gulp.series("html", "htmlinclude", sync.reload));
 };
 
 exports.default = gulp.series(
