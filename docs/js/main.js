7957e59afb1fd4a5dff01de71b9b29d12af66ad2
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-10-23 23:17:56 +0300

    dynamic accordion with data from JSON

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,18 +4,15 @@
 
 const openModalButton = document.querySelector(".details__button");
 const closeModalButton = document.querySelector(".modal__close");
-const closeSuccessModalButton = document.querySelector(".modal-success__close");
 const modal = document.querySelector(".modal");
 const modalForm = document.querySelector(".modal__form");
-const modalSuccess = document.querySelector(".modal-success");
-const body = document.querySelector(".page")
+const body = document.querySelector(".page");
 
 const openModal = () => {
     modal.classList.remove("hidden");
     closeModalButton.addEventListener("click", closeModal);
     document.addEventListener("keyup", escPressHandler);
     document.addEventListener("click", windowClickHandler);
-    modalForm.addEventListener("submit", submitForm);
     body.style.overflow = "hidden";
 }
 
@@ -24,7 +21,6 @@ const closeModal = () => {
     closeModalButton.removeEventListener("click", closeModal)
     document.removeEventListener("keyup", escPressHandler);
     document.removeEventListener("click", windowClickHandler);
-    modalForm.removeEventListener("submit", submitForm);
     modalForm.reset();
     body.style.overflow = "visible";
 }
@@ -34,7 +30,7 @@ const escPressHandler = (evt) => {
         closeModal();
     }
     if (evt.code === `Escape`) {
-        closeSuccessModal();
+        window.server.closeThanksModal();
     }
 }
 
@@ -43,39 +39,11 @@ const windowClickHandler = (evt) => {
     if (target === modal) {
         closeModal();
     }
-    if (target === modalSuccess) {
-        closeSuccessModal();
-    }
 }
 
 openModalButton.addEventListener("click", openModal)
 
-const openSuccessModal = () => {
-    modalSuccess.classList.remove("hidden");
-    closeSuccessModalButton.addEventListener("click", closeSuccessModal);
-    document.addEventListener("keyup", escPressHandler);
-    document.addEventListener("click", windowClickHandler);
-    body.style.overflow = "hidden";
+window.main = {
+  closeModal,
+  escPressHandler,
 }
-
-const closeSuccessModal = () => {
-    modalSuccess.classList.add("hidden");
-    closeSuccessModalButton.removeEventListener("click", closeSuccessModal);
-    document.addEventListener("keyup", escPressHandler);
-    document.addEventListener("click", windowClickHandler);
-    body.style.overflow = "visible";
-}
-
-const submitForm = (evt) => {
-    evt.preventDefault();
-    closeModal();
-    openSuccessModal();
-    modalForm.reset();
-    catalogForm.reset();
-}
-
-// Form
-
-const catalogForm = document.querySelector(".catalog__form")
-
-catalogForm.addEventListener("submit", submitForm);
